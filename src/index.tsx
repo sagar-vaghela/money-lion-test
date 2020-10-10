7a53903f865d0e02c9dbc3f553d7aa6eb5fca162
Sagar Vaghela <sagar19vaghela.sv@gmail.com>
2020-10-10 23:39:24 +0530

    implenented redux in main file and connect

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import "antd/dist/antd.css";
+import reducer from "./store/reducers/reducer";
+import { createStore,applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+const store: any = createStore(reducer,composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
