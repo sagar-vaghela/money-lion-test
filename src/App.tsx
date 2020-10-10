a5304e9a3361af8903a076b4db620653e9503dd8
Sagar Vaghela <sagar19vaghela.sv@gmail.com>
2020-10-10 23:40:09 +0530

    set component into main app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Welcome } from './components/welcome/Welcome';
+import Personal from './components/personal/Personal';
+import DOB from './components/dob/DOB';
+import Agreement from './components/agreement/Agreement';
+import { Header } from './components/header/Header';
+import { Container } from "semantic-ui-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header />
+      <Welcome />
+      {/* <Personal/> */}
+      {/* <DOB /> */}
+      {/* <Agreement /> */}
+    </Container>
   );
 }
 
