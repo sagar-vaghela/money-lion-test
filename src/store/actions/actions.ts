31fa90ef12ff859cbb3ba4d8fb50a14bf61938ed
Sagar Vaghela <sagar19vaghela.sv@gmail.com>
2020-10-11 13:27:33 +0530

    add reducer and action

---
+++ src/store/actions/actions.ts
@@ -0,0 +1,16 @@
+
+export const currentCompoent=(val: any)=>{
+    return { type: "CURRENT_COMPONENT", value: val };
+}
+export const changeValues=(val: any)=>{
+    return { type: "CHANGE_VALUE", value: val };
+}
+
+
+
+
+
+
+
+
+
